ce190dcbb44c68866220ef6e3c8e798a6a37d185
Master-Infrit <matheus.sam.pires@gmail.com>
2025-04-18 01:54:39 +0000

    testando

---
+++ teste.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero1, numero2;
+    int resultado;
+
+    printf("fale o numero1: \n");
+    scanf( %d &numero1);
+
+
+    return 0;
+
+
+
+
+
+
+
+}
