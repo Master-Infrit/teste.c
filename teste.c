f3e3b0d55fed82e4c27110f84cd473186d8651e1
Master-Infrit <matheus.sam.pires@gmail.com>
2025-05-28 01:50:40 +0000

    testes

--- teste.c
+++ teste.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 
 int main(){
-    int numero1;
-    int numero2;
-    numero1 = 30;
-    numero2 = 25;
+    float temperatura, umidade;
+    unsigned int estoque, estoqueMinimo = 1000;
 
-    if (numero1 >= numero2){
-        printf("é o numero  1 maior que o numero 2\n");
-  
+    printf("Entre com a temperatura: \n");
+    scanf("%f", &temperatura);
+    printf("Entre com a umidade: \n");
+    scanf("%f", &umidade);
+    printf("entre com o estoque: \n")
+    scanf("%u", &estoque);
+
+    if ( temperatura < 30){
+        printf("Temperatura esta alta\n");
+    else {
+        printf("Temperatura esta nos parametros\n");
     }
-    else ()
 
+    if (umidade > 50){
+        printf("umidade elevada \n");    
+    }
+        else {
+        printf("Umidade esta normal\n");
+    }
+    
+    if (estoque < estoqueMinimo){
+        printf("Estoque abaixo do minimo");
+    }    
+    else {
+        printf("Estoque Normal! \n");
+    }
 }
